e5d885c6826de142b5715819e41daea552ba0d0a
desencuentro <->
2021-10-18 22:38:40 -0300

    idDeNodoAlias tiene parámetro crear (eleva excepción si es False y no existe)

--- src/grafosimple.py
+++ src/grafosimple.py
@@ -11,14 +11,17 @@ class GrafoSimple:
         return self._cantidadArcos
 
     def insertarArcoConAlias(self, aliasOrigen, aliasDestino, peso):
-        self.idDeNodoAlias(aliasOrigen)
-        self.idDeNodoAlias(aliasDestino)
+        self.idDeNodoAlias(aliasOrigen, crear=True)
+        self.idDeNodoAlias(aliasDestino, crear=True)
         self._cantidadArcos += 1
 
-    def idDeNodoAlias(self, aliasNodo):
+    def idDeNodoAlias(self, aliasNodo, crear=False):
+        """Devuelve el id del nodo asociado al alias, si no existe lo crea."""
         if aliasNodo in self._listaAlias:
             return self._listaAlias[aliasNodo]
         else:
+            if not crear:
+                raise Exception("No existe el alias del nodo")
             nuevoId = self._cantidadNodos
             self._cantidadNodos += 1
             self._listaAlias[aliasNodo] = nuevoId
