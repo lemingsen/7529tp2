582f5a93d1ad5bdd5477710fd01b578c841dfd78
desencuentro <->
2021-10-18 23:32:51 -0300

    test_AB10_distancias (algoritmo completo con distancias)

--- src/dijkstra.py
+++ src/dijkstra.py
@@ -1,5 +1,32 @@
+import math
+
 class Dijkstra:
     def __init__(self,grafo,desde):
         cantNodos = grafo.cantidadNodos()
         if (desde < 0) or (desde >= cantNodos):
             raise Exception("El nodo no existe")
+        
+        self._estadoVisitado = [False    for i in range(cantNodos)]
+        self._visitables     = [i        for i in range(cantNodos)]
+        self.distancias      = [math.inf for i in range(cantNodos)]
+        self.distancias[desde] = 0
+        
+        while(len(self._visitables)):
+            visitando = self._visitar()
+            arcos = grafo.arcoDesdeNodoId(visitando)
+            for(destino, peso) in arcos:
+                if not self._fueVisitado(destino):
+                    distanciaAlternativa = self.distancias[visitando] + peso
+                    if distanciaAlternativa < self.distancias[destino]:
+                        self.distancias[destino] = distanciaAlternativa
+
+    def _visitar(self):
+        # Ordenar visitables de menor a mayor distancia
+        self._visitables = sorted(self._visitables, key=lambda nodo:self.distancias[nodo])
+
+        aVisitar = self._visitables.pop(0)
+        self._estadoVisitado[aVisitar] = True
+        return aVisitar
+
+    def _fueVisitado(self, nodo):
+        return self._estadoVisitado[nodo]
--- tests/test_dijkstra.py
+++ tests/test_dijkstra.py
@@ -1,5 +1,7 @@
-import unittest
+import math
 import types
+import unittest
+
 from src.grafosimple import GrafoSimple
 from src.dijkstra import Dijkstra
 
@@ -16,4 +18,12 @@ class TestDijkstra(unittest.TestCase):
         self.assertTrue(isinstance(Dijkstra(grafo,0),Dijkstra))
         self.assertTrue(isinstance(Dijkstra(grafo,1),Dijkstra))
         self.assertRaises(Exception,Dijkstra,grafo,2)
-        
+    
+    def test_AB10_distancias(self):
+        grafo = GrafoSimple()
+        grafo.insertarArcoConAlias("A","B",10)
+        dijkstra0 = Dijkstra(grafo,0)
+        dijkstra1 = Dijkstra(grafo,1)
+
+        self.assertEqual(list(dijkstra0.distancias), [0, 10])
+        self.assertEqual(list(dijkstra1.distancias), [math.inf, 0])
