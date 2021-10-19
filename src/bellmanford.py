9eb3b1b7faa2c567dd2171b7e4453ed126e9ecf7
desencuentro <->
2021-10-19 01:02:41 -0300

    test_AB5 (pasa porque distancias es siempre 0 para cada nodo

--- src/bellmanford.py
+++ src/bellmanford.py
@@ -1,3 +1,7 @@
 class BellmanFordConCeroAgregado:
     def __init__(self,grafo):
-        raise Exception("")
+        cantNodos = grafo.cantidadNodos()
+        if cantNodos<1:
+            raise Exception("")
+        self.distancias = [0 for i in range(cantNodos)]
+
--- tests/test_bellmanford.py
+++ tests/test_bellmanford.py
@@ -7,3 +7,8 @@ class TestBellmanFordCero(unittest.TestCase):
     def test_vacio(self):
         grafo = GrafoSimple()
         self.assertRaises(Exception,BellmanFordConCeroAgregado,grafo)
+
+    def test_AB5(self):
+        grafo = GrafoSimple()
+        grafo.insertarArcoConAlias("A","B",5)
+        self.assertEqual(BellmanFordConCeroAgregado(grafo).distancias, [0, 0])
