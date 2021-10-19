99383667b3a93f8ef74062a515b163f0d298982b
desencuentro <->
2021-10-19 06:50:10 -0300

    Implementado lector

--- src/lector.py
+++ src/lector.py
@@ -2,7 +2,25 @@ from src.grafosimple import GrafoSimple
 
 class Lector:
     def __init__(self, nombreArchivo):
-        with open(nombreArchivo) as archivo:
+        with open(nombreArchivo, "r") as archivo:
             self.grafo = GrafoSimple()
+            lineas = self._lineas(archivo)
+            entradas = (self._parsear(linea) for linea in lineas if linea)
+            for entrada in entradas:
+                if entrada:
+                    self.grafo.insertarArcoConAlias(entrada[0], entrada[1], int(entrada[2]))
 
+    def _lineas(self,archivo):
+        while True:
+            linea = archivo.readline()
+            linea = linea.strip() if linea else None
+            if linea:
+                yield linea
+            else:
+                return
 
+    def _parsear(self,linea):
+        campos = linea.split(',')
+        if (not campos) or (0 == len(campos)):
+            return None
+        return list(map(lambda campo: campo.strip(), campos))
