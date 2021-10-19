2e7a50b70ebe64a5c8620bc689e24e1ac8a6f9fc
desencuentro <->
2021-10-19 02:59:05 -0300

    test_AB0BA0

--- tests/test_johnson.py
+++ tests/test_johnson.py
@@ -8,3 +8,12 @@ class TestJohnson(unittest.TestCase):
         grafo = GrafoSimple()
         self.assertRaises(Exception,Johnson,grafo)
 
+    def test_AB0BA0(self):
+        grafo = GrafoSimple()
+        grafo.insertarArcoConAlias("A","B",0)
+        grafo.insertarArcoConAlias("B","A",0)
+        self.assertEqual(len(Johnson.matriz), 2)
+        self.assertEqual(len(Johnson.matriz[0]), 2)
+        self.assertEqual(len(Johnson.matriz[1]), 2)
+        self.assertEqual(Johnson.matriz[0], [0,0])
+        self.assertEqual(Johnson.matriz[1], [0,0])
