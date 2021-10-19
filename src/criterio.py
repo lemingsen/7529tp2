10198293e5bc22c8639b6853467596aaf8022d39
desencuentro <->
2021-10-19 13:30:31 -0300

    Sólo da excepción si matriz no es array de 1x1 mínimo

--- src/criterio.py
+++ src/criterio.py
@@ -1,3 +1,5 @@
 class Criterio:
     def __init__(self, matriz):
+        if (not matriz) or (0 == len(matriz)) or (not matriz[0]) or (0 == len(matriz[0])):
+            raise Exception("El formato de la matriz no es corrrecto")
         pass
