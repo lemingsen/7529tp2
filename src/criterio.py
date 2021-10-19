e947ab4985bc7e3fb76cb242eed4232b9a5e487f
desencuentro <->
2021-10-19 14:47:56 -0300

    .analisis

--- src/criterio.py
+++ src/criterio.py
@@ -4,7 +4,9 @@ class Criterio:
     def __init__(self, matriz):
         if (not matriz) or (0 == len(matriz)) or (not matriz[0]) or (0 == len(matriz[0])):
             raise Exception("El formato de la matriz no es corrrecto")
+        self.analisis = list(map(Criterio.analizar,matriz))
         self.mejores = [0]
+
 
     @classmethod
     def analizar(cls, distancias):
@@ -15,7 +17,11 @@ class Criterio:
                 infs += 1
             else:
                 suma += distancia
-        if infs >= len(distancias):
+        validos = len(distancias) - infs
+        if validos > 0:
+            suma = suma / validos
+        else:
             suma = inf
-
+        
         return (infs,suma)
+
--- tests/test_criterio.py
+++ tests/test_criterio.py
@@ -26,5 +26,14 @@ class TestCriterio(unittest.TestCase):
         self.assertEqual(Criterio.analizar([inf,20,inf]), (2,20))
 
     def test_analizar_inf20inf(self):
-        self.assertEqual(Criterio.analizar([-6,inf,13,inf]), (2,7))
+        self.assertEqual(Criterio.analizar([-6,inf,13,inf]), (2,3.5))
+
+    def test_analisis(self):
+        self.assertEqual(Criterio([ [inf,inf], [inf,inf] ]).analisis, [(2,inf),(2,inf)])
+        self.assertEqual(Criterio([ [inf,inf], [inf,  9] ]).analisis, [(2,inf),(1,  9)])
+        self.assertEqual(Criterio([ [  9,inf], [inf,inf] ]).analisis, [(1,  9),(2,inf)])
+        self.assertEqual(Criterio([ [ -6,inf], [inf,  5] ]).analisis, [(1, -6),(1,  5)])
+        self.assertEqual(Criterio([ [  1,  5], [  2,  2] ]).analisis, [(0,  3),(0,  2)])
+        variosDe3 = [ [6,inf,2], [inf,inf,0], [3,5,7], [inf,inf,inf] ]
+        self.assertEqual(Criterio(variosDe3).analisis, [(1,4),(2,0),(0,5),(3,inf)])
 
