fc9d11e18dad12e294d3714bd0d7f1aa29809179
desencuentro <->
2021-10-18 22:49:18 -0300

    +test_AB10_adyctentes

--- src/grafosimple.py
+++ src/grafosimple.py
@@ -3,6 +3,7 @@ class GrafoSimple:
         self._cantidadNodos = 0
         self._cantidadArcos = 0
         self._listaAlias = dict()
+        self._arcosDesdeOrigen = []
 
     def cantidadNodos(self):
         return self._cantidadNodos
@@ -11,18 +12,30 @@ class GrafoSimple:
         return self._cantidadArcos
 
     def insertarArcoConAlias(self, aliasOrigen, aliasDestino, peso):
-        self.idDeNodoAlias(aliasOrigen, crear=True)
-        self.idDeNodoAlias(aliasDestino, crear=True)
+        desde = self.idDeNodoAlias(aliasOrigen, crear=True)
+        hasta = self.idDeNodoAlias(aliasDestino, crear=True)
+        arco = (hasta, peso)
+        self._arcosDesdeOrigen[desde].append(arco)
         self._cantidadArcos += 1
 
     def idDeNodoAlias(self, aliasNodo, crear=False):
-        """Devuelve el id del nodo asociado al alias, si no existe lo crea."""
+        """Devuelve el id del nodo asociado al alias.
+        Si no existe lo crea (si crear=True) o eleva una excepción."""
         if aliasNodo in self._listaAlias:
             return self._listaAlias[aliasNodo]
+
         else:
             if not crear:
                 raise Exception("No existe el alias del nodo")
             nuevoId = self._cantidadNodos
             self._cantidadNodos += 1
             self._listaAlias[aliasNodo] = nuevoId
+            self._arcosDesdeOrigen.append([])
             return nuevoId
+
+    def arcoDesdeNodoId(self, idOrigen):
+        if (idOrigen <0) or (idOrigen >= self.cantidadNodos()):
+            raise Exception("El id de origen del nodo no existe")
+        return self._arcosDesdeOrigen[idOrigen]
+
+
