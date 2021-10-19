718f919e9a3d8b20075d3f1bb67b66b38ffe9153
desencuentro <->
2021-10-19 02:04:38 -0300

    +test_2rondas

--- tests/test_bellmanford.py
+++ tests/test_bellmanford.py
@@ -20,3 +20,15 @@ class TestBellmanFordCero(unittest.TestCase):
         grafo.insertarArcoConAlias("C","A",20)
         self.assertEqual(BellmanFordConCeroAgregado(grafo).distancias, [0, -5, -15])
 
+    def test_2rondas(self):
+        grafo = GrafoSimple()
+        grafo.insertarArcoConAlias("A","B",-2)
+        grafo.insertarArcoConAlias("C","D",-2)
+        grafo.insertarArcoConAlias("A","D",-1)
+        grafo.insertarArcoConAlias("B","C",5)
+        grafo.insertarArcoConAlias("D","B",-3)
+        distancias = BellmanFordConCeroAgregado(grafo).distancias
+        self.assertEqual(distancias[grafo.idDeNodoAlias("A")], 0)
+        self.assertEqual(distancias[grafo.idDeNodoAlias("B")], -5)
+        self.assertEqual(distancias[grafo.idDeNodoAlias("C")], 0)
+        self.assertEqual(distancias[grafo.idDeNodoAlias("D")], -2)
