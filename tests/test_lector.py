3007c9a763ab2d0b15de2c3b62e592ffefb8e0f9
desencuentro <->
2021-10-19 05:07:50 -0300

    +test_vacio, +pathArchivo

--- tests/test_lector.py
+++ tests/test_lector.py
@@ -1,10 +1,23 @@
+import os
 import unittest
 
 from src.lector import Lector
 
 class TestLector(unittest.TestCase):
+    def pathArchivo(self, rutaRelativa):
+        return os.path.join(os.path.dirname(__file__), rutaRelativa)
+
     def test_inexistente(self):
-        self.assertRaises(Exception,Lector,"nombre_de_archivo_inexistente.txt")
+        rutaArchivo = self.pathArchivo("entradas/nombre_de_archivo_inexistente.txt")
+        self.assertRaises(Exception,Lector,rutaArchivo)
+
+    def test_vacio(self):
+        rutaArchivo = self.pathArchivo("entradas/archivo_vacio.txt")
+        grafo = Lector(rutaArchivo).grafo
+        self.assertEqual(0,grafo.cantidadNodos())
+        self.assertEqual(0,grafo.cantidadArcos())
+        self.assertEqual(0,len(list(grafo.arcos())))
+
 
 
 if __name__ == '__main__':
