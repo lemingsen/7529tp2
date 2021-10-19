32c276de151278ba90960468b108aeea9e35c66f
desencuentro <->
2021-10-18 22:14:57 -0300

    cantidadArcos y cantidadNodos incrementa

--- src/grafosimple.py
+++ src/grafosimple.py
@@ -1,10 +1,16 @@
 class GrafoSimple:
     def __init__(self):
-        pass
+        self._cantidadNodos = 0
+        self._cantidadArcos = 0
 
     def cantidadNodos(self):
-        return 0
+        return self._cantidadNodos
 
     def cantidadArcos(self):
-        return 0
+        return self._cantidadArcos
+
+    def insertarArcoConAlias(self, aliasOrigen, aliasDestino, peso):
+        self._cantidadNodos += 2
+        self._cantidadArcos += 1
+
 
