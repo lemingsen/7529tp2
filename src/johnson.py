422cee86e7057cc943d50b930a30df4dd6ad74e1
desencuentro <->
2021-10-19 03:06:24 -0300

    Matriz usa Dijkstra iterativos

--- src/johnson.py
+++ src/johnson.py
@@ -1,5 +1,9 @@
+from src.dijkstra import Dijkstra
+
 class Johnson:
     def __init__(self, grafo):
         if grafo.cantidadNodos()<1:
             raise Exception("No hay suficientes nodos")
-        self.matriz = []
+
+        self.matriz = [Dijkstra(grafo, u).distancias
+                        for u in range(grafo.cantidadNodos())]
